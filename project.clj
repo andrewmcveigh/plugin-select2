8d136233f8d30a810833aa742f0d45376bf231b6
Andrew Mcveigh <me@andrewmcveigh.com>
2013-07-01 21:32:23 +0200

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.andrewmcveigh/plugin-select2 "0.1.0-SNAPSHOT"
+(defproject com.andrewmcveigh/plugin-select2 "0.1.0"
   :description "Select2 packaged as a Servlet 3.0 plugin. With a couple of
                clojure/hiccup helpers."
   :url "http://github.com/andrewmcveigh/plugin-select2"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.andrewmcveigh/plugin-jquery "0.1.0-SNAPSHOT"]]
+                 [com.andrewmcveigh/plugin-jquery "0.1.0"]]
   :repositories [["snapshots" {:url "https://clojars.org/repo/" :creds :gpg}]
                  ["releases" {:url "https://clojars.org/repo/" :creds :gpg}]])
