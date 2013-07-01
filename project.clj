b9749f2668295a0fa28f97488d7c26dcd072a20f
Andrew Mcveigh <me@andrewmcveigh.com>
2013-07-01 20:56:57 +0200

    Add clojars as snapshots/releases

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.andrewmcveigh/plugin-jquery "0.1.0-SNAPSHOT"]])
+                 [com.andrewmcveigh/plugin-jquery "0.1.0-SNAPSHOT"]]
+  :repositories [["snapshots" {:url "https://clojars.org/repo/" :creds :gpg}]
+                 ["releases" {:url "https://clojars.org/repo/" :creds :gpg}]])
